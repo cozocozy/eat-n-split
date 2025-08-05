7266fa16b07d03a49d1601e03f69e7ce79c242a5
septian <cozocozy@gmail.com>
2025-08-05 12:06:20 +0700

    feat:add new form add friend

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Image from "next/image";
 
 const initialFriends = [
@@ -20,10 +21,97 @@ const initialFriends = [
     balance: 0,
   },
 ];
-export default function Home() {
+function Button({ children, onClick }) {
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default function App() {
+  const [showForm, setShowForm] = useState(false);
+  function handleShowAddFriend() {
+    setShowForm((show) => !show);
+  }
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <h1>Friends List</h1>
+        <FriendList friends={initialFriends} />
+        {showForm && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showForm ? "Close" : "Add Friend"}
+        </Button>
+      </div>
+      <FormSplitBill />
+    </div>
+  );
+}
+
+function FriendList({ friends }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+}
+function Friend({ friend }) {
+  return (
+    <li>
+      <Image src={friend.image} alt={friend.name} width={48} height={48} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          you owes{friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${friend.balance}
+        </p>
+      )}
+      {friend.balance === 0 && <p>you and {friend.name} are even</p>}
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🎎 Friend Name</label>
+      <input type="text" />
+      <label>🧧 Image URL</label>
+      <input type="text" />
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with a friend</h2>
+
+      <label>💰 Bill Value</label>
+      <input type="number" />
+
+      <label>💰 Your expense</label>
+      <input type="number" />
+
+      <label>💰 X's expense</label>
+      <input type="number" disabled />
+
+      <label>💰 Who's paying bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">Select a friend</option>
+      </select>
+
+      <Button>Split Bill</Button>
+    </form>
   );
 }
